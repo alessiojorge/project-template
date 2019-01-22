77ed5560dc21d1f45dd03233197c8fcaec5af1c3
Tarma <tarmakatz@gmail.com>
2019-01-22 17:27:30 +0100

    made further changes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,8 +1,5 @@
 import React, { Component, Fragment } from 'react';
 
-//  imports for components
-import { Header, Content, Footer, Navigation } from '../components';
-
 class App extends Component {
   render() {
     return <Fragment />;
